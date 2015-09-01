9433d362393987492822cb6bd23589056255a502
Румен Драганов <r.draganov@respect93.com>
2015-09-01 22:57:13 +0300

    Модул 06 Видео 02 03 dll файлове. Архитектура на програмата

---
+++ MP01/Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator
+{
+	class MainClass
+	{
+		public UserInput.UInput uInput=new UserInput.UInput();
+
+		public static void Main (string[] args)
+		{
+
+
+			MainClass _program = new MainClass ();
+			_program.uInput.JustTesting ();
+		}
+	}
+}
---
+++ MP01/UserInput/UserInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UserInput
+{
+	public class UInput	
+	{
+		public UInput ()
+		{
+		}
+		public void JustTesting()
+		{
+			Console.WriteLine ("Стартиране на UserInput.UInput.JustTesting()");
+		}
+	}
+}
+
